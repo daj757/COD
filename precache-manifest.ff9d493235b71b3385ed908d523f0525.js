8dd7dd0173e39dc961f8cb9107cdb269d645b76b
Daniel Jimenez <dj@MacBook-Pro.local>
2020-06-08 18:11:31 -0600

    Updates

---
+++ precache-manifest.ff9d493235b71b3385ed908d523f0525.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1f82345e59e32d8d283c95f5898380",
+    "url": "/COD/index.html"
+  },
+  {
+    "revision": "4bf3d1c5a3a9c85691d6",
+    "url": "/COD/static/css/2.3a0c9139.chunk.css"
+  },
+  {
+    "revision": "fc84d5d6438d0c30066f",
+    "url": "/COD/static/css/main.6d9a40de.chunk.css"
+  },
+  {
+    "revision": "4bf3d1c5a3a9c85691d6",
+    "url": "/COD/static/js/2.fb5d1fbe.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/COD/static/js/2.fb5d1fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc84d5d6438d0c30066f",
+    "url": "/COD/static/js/main.7dd0ee3f.chunk.js"
+  },
+  {
+    "revision": "24e938790a1f7a1d9ffc",
+    "url": "/COD/static/js/runtime-main.4a5db9e0.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/COD/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/COD/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/COD/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/COD/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/COD/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "4f95f89400a24dc2177a4660255c2a27",
+    "url": "/COD/static/media/david.4f95f894.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/COD/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "3f0d04a2c7abbdc154d0ca0131ebe9bb",
+    "url": "/COD/static/media/font.3f0d04a2.ttf"
+  },
+  {
+    "revision": "2edc5ab821455eaf1ac6b868829543c0",
+    "url": "/COD/static/media/hands.2edc5ab8.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/COD/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/COD/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/COD/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/COD/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/COD/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "874e9af90dfda1b5c14f685b790c4000",
+    "url": "/COD/static/media/landing.874e9af9.jpg"
+  },
+  {
+    "revision": "3c6aaadc7a3aa9052d20e549d8a78364",
+    "url": "/COD/static/media/landingMobile.3c6aaadc.jpg"
+  },
+  {
+    "revision": "bfaf3ce7ceb1b0362a7b23059f4f85cf",
+    "url": "/COD/static/media/logo.bfaf3ce7.png"
+  },
+  {
+    "revision": "4814b45771cfde8df0716ab30982947c",
+    "url": "/COD/static/media/nasa.4814b457.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/COD/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/COD/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/COD/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/COD/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/COD/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
